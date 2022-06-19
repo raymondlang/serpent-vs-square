a0f04d73343c6bd96a3e9281d2ef5e0e69984930
Raymond <raymond.w.lang@gmail.com>
2022-06-19 10:06:15 -0700

    started game view

--- dist/bundle.js
+++ dist/bundle.js
@@ -403,3 +403,46 @@ Game.BG_COLOR = "#000000";
 Game.DIM_X = 400;
 Game.DIM_Y = 850;
 // Game.FPS = 60;
+
+module.exports = Game;
+
+/***/ }),
+
+/***/ "./lib/game_view.js":
+/*!**************************!*\
+  !*** ./lib/game_view.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+const keyCodes = {
+  left: [-3, 0],
+  right: [3, 0]
+};
+
+// console.log(pauseButton);
+const pauseButton = document.getElementById("pausebtn");
+
+class GameView {
+  constructor(game, ctx) {
+    this.ctx = ctx;
+    this.game = game;
+    this.serpent = this.game.addSerpent();
+    this.keys = Object.create(null);
+    // window.addEventListener('keydown', this.bindKeyHandlers);
+    this.gameStarted = false;
+    // console.log(pauseButton);
+    this.paused = false;
+    this.time = false;
+
+    pauseButton.addEventListener('click', this.togglePlay.bind(this));
+
+    this.togglePlay = this.togglePlay.bind(this);
+
+    // this.game.addBlocks();
+    // this.game.addCircles();
+    // this.game.addLines();
+    window.addEventListener('keyup', this.resetSerp.bind(this));
+    this.resetSerp = this.resetSerp.bind(this);
+
+  }
